91f3ebd4cbe539179a3d667a2f01700a21584ba6
Everton <evertonulisystem@gmail.com>
2021-10-15 23:04:29 -0300

    Criação da interface do Repository

---
+++ apiRest-produto/src/main/java/br/gotasdetecnologia/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.gotasdetecnologia.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gotasdetecnologia.apirest.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+
+}
