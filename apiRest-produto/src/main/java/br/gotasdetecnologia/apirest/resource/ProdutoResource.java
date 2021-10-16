91429022b506faff2989689570c931f8e2ce8e06
Everton <evertonulisystem@gmail.com>
2021-10-16 20:41:21 -0300

    CRUD completo

--- apiRest-produto/src/main/java/br/gotasdetecnologia/apirest/resource/ProdutoResource.java
+++ apiRest-produto/src/main/java/br/gotasdetecnologia/apirest/resource/ProdutoResource.java
@@ -2,10 +2,15 @@ package br.gotasdetecnologia.apirest.resource;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.gotasdetecnologia.apirest.model.Produto;
 import br.gotasdetecnologia.apirest.repository.ProdutoRepository;
 
-@RestController
+@RestController //Aqui a diferença da aplicação com Model. Ou seja, retorna um objeto e não uma visão. dispensa uso do ResponseBody. E enviaremos o obje de dominio como resp. HTTP no formato JSON ( entendido para consumo do postman por exe)
 @RequestMapping(value="/api")
 public class ProdutoResource {
 	
@@ -30,10 +35,26 @@ public class ProdutoResource {
 		return produtoRepository.findById(id);
 	}
 	
-	@PostMapping(path= "/produto")
+	@PostMapping(path="/produto")
 	public Produto salvarProduto(@RequestBody Produto produto){
 		return produtoRepository.save(produto);
 	}
+	
+	@DeleteMapping(path="/produto") //o ID é passado no proprio postman
+	//@Transactional
+	public void excluirRegistroPorId(@RequestBody Produto produto){
+		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping(path="/produto") //o ID é passado no proprio postman
+	//@Transactional
+	public Produto atualizarRegistroPorId(@RequestBody Produto produto){
+		return produtoRepository.save(produto);
+	}
+	
+	
+	
+	
 	
 	
 
