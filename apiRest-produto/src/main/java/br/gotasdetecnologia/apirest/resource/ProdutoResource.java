8cc293622e7b7fab68c2ff4398749833a79cd016
Everton <evertonulisystem@gmail.com>
2021-10-15 23:21:20 -0300

    Criação do resource da API - RestController (Listagem do Produto)

---
+++ apiRest-produto/src/main/java/br/gotasdetecnologia/apirest/resource/ProdutoResource.java
@@ -0,0 +1,25 @@
+package br.gotasdetecnologia.apirest.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gotasdetecnologia.apirest.model.Produto;
+import br.gotasdetecnologia.apirest.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaprodutos(){
+		return produtoRepository.findAll();
+	}
+
+}
