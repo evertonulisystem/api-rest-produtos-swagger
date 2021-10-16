45b68312b9e52872e1c9f3986cc61d1d57f4c533
Everton <evertonulisystem@gmail.com>
2021-10-15 22:55:16 -0300

    Criação da Entidade Produto

---
+++ apiRest-produto/src/main/java/br/gotasdetecnologia/apirest/model/Produto.java
@@ -0,0 +1,64 @@
+package br.gotasdetecnologia.apirest.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_Produto")
+public class Produto implements Serializable{
+	private static final long serialVersionUID= 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO) //Criando automaticamente os ID; No POST não precisa passar o id
+	private long id;
+	
+	private String nome;
+	
+	private BigDecimal quantidade;
+	
+	private BigDecimal preco;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
+	
+
+}
